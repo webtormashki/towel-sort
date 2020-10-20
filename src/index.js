5895b34d5d7a20bba156698143e7afce7dd9e9ac
Tanya <webtormashki@gmail.com>
2020-10-21 02:35:30 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (!matrix) {
+        return [];
+    }
+
+    var sum = [];
+    for (i = 0; i < matrix.length; i++) {
+        var array = matrix[i];
+        if (i % 2 != 0) {
+            array.sort(function(a, b) {
+            return b - a;
+        });
+    }
+    sum = sum.concat(array);
+  }
+  return sum;
 }
